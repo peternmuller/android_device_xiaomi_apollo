bb29cbdb4fb0fba73cf2e6cbb60068eb5f4895f0
xboxfanj <xboxlover360@gmail.com>
2016-01-16 18:45:48 +0100

    apollo: Clean up kernel cmdline
     * Console is overwritten by the bootloader
     * CONFIG_DEBUG_USER is disabled, so no need to keep user_debug
       and the same goes for msm_rtb, which depends on CONFIG_MSM_RTB
    
    Change-Id: I9a30f32f7dc91cf82f2516bf6a54d7d62f1c00f1
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,17 +83,14 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
 
 # Kernel
 BOARD_KERNEL_CMDLINE := \
-    androidboot.console=ttyMSM0 \
     androidboot.hardware=qcom \
     androidboot.init_fatal_reboot_target=recovery \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     cgroup.memory=nokmem,nosocket \
-    console=ttyMSM0,115200n8 \
     earlycon=msm_geni_serial,0xa90000 \
     loop.max_part=7 \
     lpm_levels.sleep_disabled=1 \
-    msm_rtb.filter=0x237 \
     reboot=panic_warm \
     service_locator.enable=1 \
     swiotlb=2048
