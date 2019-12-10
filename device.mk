79afc74596767103a214b66f8407400ac25d6994
justinweng <justinweng@google.com>
2019-12-10 18:31:56 +0800

    apollo: Enforce 24-bit audio for offload playback
    Test: Audio Playback
    
    Change-Id: Ia581e65a0e5e8dcbfae188fe36ca9d07bd9eb4e2
    Signed-off-by: justinweng <justinweng@google.com>
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -73,7 +73,8 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.audio.resampler.psd.halflength=960 \
     ro.audio.resampler.psd.cutoff_percent=98 \
     ro.audio.resampler.psd.tbwcheat=0 \
-    persist.bluetooth.sbc_hd_higher_bitrate=1
+    persist.bluetooth.sbc_hd_higher_bitrate=1 \
+    persist.vendor.audio_hal.dsp_bit_width_enforce_mode=24
 
 PRODUCT_ODM_PROPERTIES += \
     aaudio.mmap_policy=1 \
