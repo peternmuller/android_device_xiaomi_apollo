dbe2ba03c188a65b9a66a6f8fbba76019343777f
justinweng <justinweng@google.com>
2023-04-23 23:51:40 +0530

    apollo: Extend audio offload buffer size to 256kb
    Extend buffer size for offload playback to reduce the frequency of CPU
    wake up.
    It makes ADSP to process more data one time and reduce the number of
    times to wake up CPU.
    
    Bug: 137973303
    Test: MP3 playback
    
    Change-Id: Ib0b2a8827647ed780bb20bd9dfa2a383153eb282
    Signed-off-by: justinweng <justinweng@google.com>
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: Pascoato <viniciuspascoato@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -84,7 +84,8 @@ PRODUCT_ODM_PROPERTIES += \
     vendor.audio.feature.spkr_prot.enable=false \
     vendor.audio.hal.output.suspend.supported=false \
     vendor.audio.offload.multiple.enabled=true \
-    vendor.audio.offload.track.enable=false
+    vendor.audio.offload.track.enable=false \
+    vendor.audio.offload.buffer.size.kb=256
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
