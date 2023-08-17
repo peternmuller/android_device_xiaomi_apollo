79b0ea382ce7a3835fbfb2dce5b8f8ff001c772b
Vinoth Jayaram <quic_c_vinoja@quicinc.com>
2023-08-17 18:45:32 +0530

    apollo: Enabled to allow HWC path for WFD
    Change-Id: I7145ce66678901128dfb0c659580f257ce6bb9c1
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -188,7 +188,8 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.display.sensortype=2 \
     vendor.display.qdcm.mode_combine=1 \
     vendor.display.use_layer_ext=1 \
-    vendor.display.defer_fps_frame_count=2
+    vendor.display.defer_fps_frame_count=2 \
+    vendor.display.vds_allow_hwc=1
 
 PRODUCT_ODM_PROPERTIES += \
     vendor.display.use_smooth_motion=0
