91320b2a182a043a51ae5f55c81aa3b75570d47c
Sergey Volk <servolk@google.com>
2023-08-23 18:23:01 +0000

    apollo: Add a sysprop for passing errors between HWC and SF
    Test: manual
    Bug: 283461313
    Change-Id: Id18e4431d72533ba92b63fc6139abaf1cf7c7b5c
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -203,7 +203,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.frame_rate_multiple_threshold=144 \
     debug.hwui.skia_tracing_enabled=false \
     debug.hwui.skia_use_perfetto_track_events=false \
-    debug.sf.treat_170m_as_sRGB=1
+    debug.sf.treat_170m_as_sRGB=1 \
+    debug.sf.hwc_hotplug_error_via_neg_vsync=1
 
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.config.avoid_gfx_accel=true
