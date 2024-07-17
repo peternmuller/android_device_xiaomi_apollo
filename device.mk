4650a0a09b9d9066d7402935e0a48e5330090d03
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-07-17 12:53:43 +0200

    apollo: Switch to QTI USB Gadget HAL
    [chrisl7]: Adapt to 4.19 kernel
    [peternmuller]: Removed props chrisl7 introduced, but needs https://gerrit.aospa.co/c/AOSPA/android_vendor_qcom-opensource_usb/+/41609 instead.
    
    Change-Id: I48f65d0495356ce2f941e0fe13f879cf85c0e03c
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -521,6 +521,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.sys.thermal.data.path=/data/vendor/thermal/
 
+# USB
+PRODUCT_HAS_GADGET_HAL := true
+
 # Vendor
 $(call inherit-product, vendor/xiaomi/apollo/apollo-vendor.mk)
 
