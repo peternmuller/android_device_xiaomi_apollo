56e0921f80e8f5aca4963f769de4cc3b00384473
EmanuelCN <emanuelghub@gmail.com>
2024-10-16 00:10:15 +0200

    apollo: Override cgroups.json & task_profiles.json * In order to allow uclamp kernels to place tasks correctly

--- device.mk
+++ device.mk
@@ -108,6 +108,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Charging
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.cp.fcc_main_ua=400000 \
