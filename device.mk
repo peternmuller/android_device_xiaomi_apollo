7f743d772a7f146762ee5f3f9e2e9607fdcd0878
AdarshGrewal <adarshgrewal@gmail.com>
2024-10-31 09:57:44 +0100

    apollo: Build missing DRM library
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.3-service.widevine": cannot locate symbol "CBS_init" referenced by "/vendor/lib64/libwvhidl.so"...
    
    Change-Id: Ic85294b92e314452be88a32d8d35df8637df7e01
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -170,6 +170,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     drm.service.enabled=true
 
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey \
     libcrypto-v33
 
