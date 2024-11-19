9d1659f8a4dcd47f668800d1e528a001f4865204
dogpoopy <dogpoopy.git@gmail.com>
2024-11-19 03:48:30 +0000

    apollo: Disable checkelf for now
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -72,4 +72,6 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
+export TARGET_ENABLE_CHECKELF=false
+
 "${MY_DIR}/setup-makefiles.sh"
