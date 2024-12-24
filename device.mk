e369a83f9a203c41e0c21286d53f8fd5e90e034b
Shev T <evgeny.shishkov@gmail.com>
2024-12-24 15:31:33 +0800

    apollo: Switch to AIDL NXP NFC HAL
    * Drop NFC component
    * Also build libchrome.vendor
    * Also patch libnfc-nci.conf
    
    Partial commit histories at https://github.com/LineageOS/android_device_xiaomi_sm8250-common and https://github.com/QKIvan/android_device_xiaomi_apollo
    
    Change-Id: I3fce9c48aced50c77b6e81ad10e686a48c9c6395
    Co-authored-by: Łukasz Patron <priv.luk@gmail.com>
    Co-authored-by: Sebastiano Barezzi <seba@sebaubuntu.dev>
    Co-authored-by: QKIvan <ivan33@163.com>
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -244,6 +244,30 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc-service.nxp \
+    com.android.nfc_extras \
+    NfcNci \
+    SecureElement \
+    Tag
+
+PRODUCT_PACKAGES += \
+    libchrome.vendor
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.nfc.port=I2C
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.nfc.ese.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.nfc.uicc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.se.omapi.ese.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/com.nxp.mifare.xml
+
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
@@ -294,7 +318,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
     gps \
     init \
     media \
-    nfc \
     overlay \
     perf \
     telephony \
