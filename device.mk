3df2caea492ba9c2369359ca3d9c6a98a1f872b4
Alucard Stormbringer <supersayan1000@gmail.com>
2025-01-23 16:11:58 +0000

    apollo: Build libutilscallstack vendor service
    error: Could not load /vendor/lib64/hw/camera.xiaomi.so from default namespace: dlopen failed: library "libutilscallstack.so" not found: needed by /vendor/lib64/libcom.xiaomi.debug.so
    
    Signed-Off-By: kartik-commits	<kartiklashkare@gmail.com>
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -108,6 +108,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    libutilscallstack.vendor
+
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
