e81f1d8bfc9202227f21bf4517b0e49c77a0f46a
peternmuller <pm563838@gmail.com>
2025-04-22 02:01:10 +0200

    apollo: Disable Skia tracing
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -184,7 +184,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_idle_timer_ms=1000 \
     ro.surface_flinger.set_touch_timer_ms=100 \
     vendor.display.idle_time=1100 \
-    debug.sf.frame_rate_multiple_threshold=144
+    debug.sf.frame_rate_multiple_threshold=144 \
+    debug.hwui.skia_tracing_enabled=false \
+    debug.hwui.skia_use_perfetto_track_events=false
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/display/,$(TARGET_COPY_OUT_VENDOR)/etc)
